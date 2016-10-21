32b5e097802cdde3f331f2b051312e20a4accc89
as02518237 <allanmw@gmail.com>
2016-10-21 14:26:43 -0200

    iOS

--- ios/CsZBar.m
+++ ios/CsZBar.m
@@ -99,7 +99,7 @@
         [self.scanReader.view addSubview:toolbarViewFlash];
 
         if (drawSight) {
-            CGFloat dim = screenWidth < screenHeight ? screenWidth / 1.1 : screenHeight / 1.1;
+            CGFloat dim = screenWidth / 1.1;
             UIView *polygonView = [[UIView alloc] initWithFrame: CGRectMake  ( (screenWidth/2) - (dim/2), (screenHeight/2) - (dim/2), dim, dim)];
             
             UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(0,dim / 2, dim, 1)];
