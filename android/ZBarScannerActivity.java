0a5aa23825abf3becc8cd44b2fd75d004971b0b6
allanwolski <allanmw@gmail.com>
2017-07-25 16:36:58 -0300

    Update ZBarScannerActivity.java

--- android/ZBarScannerActivity.java
+++ android/ZBarScannerActivity.java
@@ -543,7 +543,7 @@ public class ZBarScannerActivity extends Activity
         if (holder != null) {
             try {
                 // 90 degrees rotation for Portrait orientation Activity.
-                setCameraDisplayOrientation(this, 0);
+                // setCameraDisplayOrientation(this, 0);
                 android.hardware.Camera.Parameters camParams = camera.getParameters();
 
                 //camParams.setFlashMode(Parameters.FLASH_MODE_TORCH);
